5e9ab44cce0a11075c5dcf479b769ea88d9a0d6f
David van Vliet <dv23@rice.edu>
2026-01-27 14:24:03 -0800

    added toolbar'

--- frontend/app/dashboard/components/Spreadsheet/index.tsx
+++ frontend/app/dashboard/components/Spreadsheet/index.tsx
@@ -1,4 +1,5 @@
 import { SpreadsheetProvider } from './SpreadsheetContext';
+import Toolbar from './Toolbar';
 import FormulaBar from './FormulaBar';
 import Grid from './Grid';
 import styles from './Spreadsheet.module.css';
@@ -7,6 +8,7 @@ export default function Spreadsheet() {
   return (
     <SpreadsheetProvider>
       <div className={styles.spreadsheet}>
+        <Toolbar />
         <FormulaBar />
         <Grid />
       </div>
