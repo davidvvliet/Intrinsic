790486bcafb40aa196b4c207d20d6f5c05a87b96
David van Vliet <dv23@rice.edu>
2026-03-16 11:28:21 -0700

    proxy change

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -30,6 +30,6 @@ export default authkitMiddleware({
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|llms.txt).*)',
   ],
 };
